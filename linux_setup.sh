59453f4969f9565a234dcb681217d1e4b6679deb
Rohit Jain <49694329+gitrohitjain@users.noreply.github.com>
2023-05-21 15:17:12 +0800

    Update linux_setup.sh

--- linux_setup.sh
+++ linux_setup.sh
@@ -1,5 +1,5 @@
 apt -y update
 apt -y upgrade
-apt install -y build-essential vim git curl wget zip unzip
+apt install -y build-essential vim git curl wget zip unzip cmake
 cd ~ && wget https://raw.githubusercontent.com/gitrohitjain/beautify-terminal/main/beautify.sh
 cd ~ && echo -Y | sh beautify.sh
